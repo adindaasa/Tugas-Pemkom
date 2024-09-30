f65f8444c870090c896e99ac0d4e972e46b78d0f
adinda0112 <dinda0112112@gmail.om>
2024-09-30 14:29:47 +0700

    first

---
+++ src/tampilan/login/TampilanLogin.java
@@ -0,0 +1,17 @@
+
+package tampilan.login;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class TampilanLogin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
